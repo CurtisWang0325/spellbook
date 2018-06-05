af9c88208cc3775497eceac97294801ae6fbbc09
Chien Wang <wangc6@rose-hulman.edu>
2018-06-05 10:14:20 -0400

    finish 6/4 hw

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('testing')
+const heading1 = document.querySelector('#head1')
+const heading2 = document.querySelector('#head2')
+const button = document.querySelector('button')
+button.addEventListener('click',(ev)=>{
+    heading2.textContent="Changed";
+})
+const submit = document.querySelector('#submit')
+submit.addEventListener('click',(ev)=>{ 
+    heading1.textContent=document.getElementById('input').value;
+})
